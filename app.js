f0759e4443b7fc549c35ac1e6e37e93880e56294
uwem-RH <99641063+uwem-RH@users.noreply.github.com>
2023-12-14 10:47:52 +0100

    update

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ const link = document.getElementById("button")
 link.addEventListener("click", ()=>{
     window.location.href = "https://hailr.app/";
 })
+
+const a = document.getElementById("location")
+a.href = location.href
