55c65fe44983c871bce11a6f4aada191706d684d
uwem-RH <99641063+uwem-RH@users.noreply.github.com>
2023-12-14 15:05:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ link.addEventListener("click", ()=>{
 
 const a = document.getElementById("location")
 a.href = location.href
+document.addEventListener("DOMContentLoaded", function() {
+    var ua = navigator.userAgent || navigator.vendor || window.opera;
+    
+    // Check if the user agent is Instagram's webview
+    if (/Instagram/.test(ua)) {
+        // Hide the button if opened in Instagram's webview
+        document.getElementById("yourButtonId").style.display = 'none';
+    }
+});
